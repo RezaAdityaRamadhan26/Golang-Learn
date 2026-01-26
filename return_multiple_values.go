2182a2bd8657fb5f1edcad5fcb9d50930ba97f0a
faizadie30 <faiz.adie30@gmail.com>
2026-01-26 17:14:45 +0700

    variadic_function

--- return_multiple_values.go
+++ return_multiple_values.go
@@ -1,23 +1,23 @@
 package main
 
-import "fmt"
+// // import "fmt"
 
-func getFullName() (firstName, middleName, lastName string) {
-	// return "Reza", "Aditya"
-	firstName = "Muhammad"
-	middleName = "Reza"
-	lastName = "Aditya"
+// func getFullName() (firstName, middleName, lastName string) {
+// 	// return "Reza", "Aditya"
+// 	firstName = "Muhammad"
+// 	middleName = "Reza"
+// 	lastName = "Aditya"
 
-	return firstName, middleName, lastName
-}
+// 	return firstName, middleName, lastName
+// }
 
-func main() {
-	// firstName, lastName := getFullName() // Memanggil 2 variable yang di return pada function getFullName
-	// fmt.Println(firstName, lastName)
+// // func main() {
+// // 	// firstName, lastName := getFullName() // Memanggil 2 variable yang di return pada function getFullName
+// // 	// fmt.Println(firstName, lastName)
 
-	// firstName, _ := getFullName()
-	// fmt.Println(firstName) // Memanggil hanya 1 variable, karna tidak bisa 1 diganti dengan "_"
+// // 	// firstName, _ := getFullName()
+// // 	// fmt.Println(firstName) // Memanggil hanya 1 variable, karna tidak bisa 1 diganti dengan "_"
 
-	firstName, middleName, lastName := getFullName()
-	fmt.Println(firstName, middleName, lastName)
-}
+// // 	firstName, middleName, lastName := getFullName()
+// // 	fmt.Println(firstName, middleName, lastName)
+// // }
