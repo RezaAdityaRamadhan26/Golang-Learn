5b0a865bbcf258dcf0d480560a58a3bd32e8c35c
faizadie30 <faiz.adie30@gmail.com>
2026-02-01 19:23:00 +0700

    closure

---
+++ closure.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	counter := 1
+
+	increment := func() {
+		counter++
+	}
+
+	increment()
+	increment()
+	increment()
+	fmt.Println(counter) // counter nya akan berubah, karna function increment mengakses variable counter di luar function increment
+						// dan merubah nilainya
+						// jadi function increment ini adalah closure
+						// karna mengakses variable di luar scope function nya
+						// dan merubah nilainya
+						// jadi hasilnya adalah 4, ditambah 3 kali pemanggilan function increment
+}
--- recursive_function.go
+++ recursive_function.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import _"fmt"
 
 func factorialLoop(value int) int {
 	result := 1
@@ -19,14 +19,14 @@ func factorialRecursive(value int) int {
 }
 
 
-func main() {
-	result := 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 // maka hasilnya akan sama dengan loop diatas
-	fmt.Println(result)
-	fmt.Println(factorialLoop(10)) // hasilnya adalah perkalian dibawah 10
-								   // 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 = 3628800
-	fmt.Println(factorialRecursive(10)) // jadi hasilnya sama dengan loop diatas, tetapi menggunakan recursive
-										// recursive adalah function yang memanggil dirinya sendiri
-										// pada contoh diatas, function factorialRecursive memanggil dirinya sendiri
-										// hingga mencapai nilai dasar yaitu 1
-										// sehingga hasilnya sama dengan loop diatas
-}
+// func main() {
+// 	result := 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 // maka hasilnya akan sama dengan loop diatas
+// 	fmt.Println(result)
+// 	fmt.Println(factorialLoop(10)) // hasilnya adalah perkalian dibawah 10
+// 								   // 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 = 3628800
+// 	fmt.Println(factorialRecursive(10)) // jadi hasilnya sama dengan loop diatas, tetapi menggunakan recursive
+// 										// recursive adalah function yang memanggil dirinya sendiri
+// 										// pada contoh diatas, function factorialRecursive memanggil dirinya sendiri
+// 										// hingga mencapai nilai dasar yaitu 1
+// 										// sehingga hasilnya sama dengan loop diatas
+// }
